9f41e43b35caf8c6552b4ddcf304d474398c3c3a
chenjiahui <mr_chenjiahui@163.com>
2020-05-25 17:30:45 +0800

    use rollup to build this package

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import sourcemaps from "rollup-plugin-sourcemaps";
+
+import pkg from "./package.json";
+
+export default {
+  input: './src/index.ts',
+  output: {
+    format: 'es',
+    file: pkg.module,
+    name: 'Rarzipano',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true, // only compile defs in es format
+          declarationDir: 'lib/types',
+          module: 'es2015',
+        },
+      },
+      useTsconfigDeclarationDir: true,
+    }),
+    resolve(),
+    sourcemaps(),
+  ],
+  external: ["react", "react-dom", "marzipano"],
+}
